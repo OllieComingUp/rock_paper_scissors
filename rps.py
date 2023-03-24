9b708f68078d315bdffbb4ee80904a5fd12ac647
ewa-szataniak <ewamariacoach@gmail.com>
2023-03-24 15:57:47 -0400

    ewa's party

--- rps.py
+++ rps.py
@@ -1,7 +1,39 @@
 import random
-name = input("What is you're name")
-print( "Hello " + name )
-choice = input("Would you like to choose rock, paper or scissors?")
-print("You choose " + choice)
+
+print("Welcome to the Rock, Paper, Scissors game!")
+name = input("What is your name?")
+print( "Hello " + name + "!")
+
+
+#list of choices for the computer
 my_random_list = ["rock", "paper", "scissors"]
-print(random.choice(my_random_list))
+# get the player's choice
+player_choice = input("Please choose rock, paper, or scissors: ").lower()
+
+#get the computer's choice
+computer_choice = random.choice(my_random_list)
+print("The computer chose "+ computer_choice)
+
+#who is a winner?
+if player_choice == computer_choice:
+    print("It's a tie!")
+
+elif ((player_choice == "rock" and computer_choice == "scissors") or (player_choice == "paper" and computer_choice =="rock") or (player_choice == "scissors" and computer_choice == "paper")):
+    print("You are a winner! Hurray!")
+
+else:
+
+    print("You lost. Better luck next time!")
+
+
+
+    
+    
+    
+
+
+
+
+
+    
+
